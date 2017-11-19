659612bccef39fcc9f5945f533b4efc2544e7f28
Jahongir Rahmonov  =) <jrahmonov2@gmail.com>
2017-11-19 21:09:01 +0500

    Add adding and editing a post

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from blog import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls'))
+    url(r'', include('blog.urls')),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
